47b1ec59e12c800eca4a9454ebddb307de960544
Nicolas  Rios <nickrios33@yahoo.com>
2019-10-21 21:29:59 -0700

    Finished Story Board. Added View Controllers, and constraints.

---
+++ ios-pt3-bw1-group-lunch-coordinator-nicolas/group.swift
@@ -0,0 +1,17 @@
+//
+//  group.swift
+//  ios-pt3-bw1-group-lunch-coordinator-nicolas
+//
+//  Created by Nicolas Rios on 10/21/19.
+//  Copyright © 2019 Nicolas Rios. All rights reserved.
+//
+
+import Foundation
+
+struct Group {
+    
+    let person: String
+    
+    let favoriteFood: String
+    
+    }
